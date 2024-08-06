85a2827c22332b3f3c6ab3f5dc7bf9499b83f469
vscoia <yeah.caio-quem@hotmail.com>
2024-08-05 22:39:34 -0300

    Correcting typos and adding compiled files

--- codigos_aula_01/ex4_integers_sum_serie.cpp
+++ codigos_aula_01/ex4_integers_sum_serie.cpp
@@ -8,7 +8,7 @@ int main() {
   for (int number = 2; number <= 20; number += 2)
     total += number;
 
-  cout << "A soma da série é" << total << endl;
+  cout << "A soma da série é " << total << endl;
  
   return 0;
 }
