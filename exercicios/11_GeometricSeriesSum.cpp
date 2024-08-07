4dc0c940906d441e9ee598e7efd952d315560d45
vscoia <yeah.caio-quem@hotmail.com>
2024-08-07 00:57:50 -0300

    added more exercises

---
+++ exercicios/11_GeometricSeriesSum.cpp
@@ -0,0 +1,4 @@
+/*
+ * Write a program called GeometricSeriesSum to compute the sum of a geometric series 1 + 1/2 + 1/4 + 1/8 + .... + 1/n.
+ * You program shall prompt for the value of n. (Hints: Use post-processing statement of denominator = denominator * 2.)
+ */
